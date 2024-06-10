96980f04bbe3be7fdb02ac111c84e4011942a421
Andriu <145680244+AndriuMaistryk12@users.noreply.github.com>
2024-06-10 16:54:28 +0300

    Add files via upload

---
+++ javascript_l1.js
@@ -0,0 +1,41 @@
+// створити змінні. Присвоїти кожній значення
+/*
+ let hello = 'hello';
+ let word = 'owu';
+ let glob_domain = 'com';
+ let ua_domain = 'ua'; 
+ let number_one = 1;
+ let number_ten = 10;
+ let big_minus = -999;
+ let numbers = 123;
+ let Pi = 3.14;
+ let height = 2.7;
+ let age = 16;
+ let nameTom = true;
+ let nameAnna = false; 
+
+console.log(hello, word, glob_domain, ua_domain, number_one, number_ten, big_minus, numbers);
+console.log(Pi, height, age, nameTom, nameAnna);
+*/
+
+
+/*Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+Вивести кожну змінну за допомогою: console.log , alert, document.write*/
+
+let hello = 'Hi!';
+ let word = 'owl';
+ let glob_domain = 'domain';
+ let ua_domain = '.ua'; 
+ let number_one = 'one';
+ let number_ten = 'ten';
+ let big_minus = -100;
+ let numbers = 456;
+ let Pi = -3.14;
+ let height = 2.5;
+ let age = 17;
+ let nameTom = true;
+ let nameAnna = false; 
+ console.log(hello, word, glob_domain, ua_domain, number_one, number_ten, big_minus, numbers);
+console.log(Pi, height, age, nameTom, nameAnna);
+
+prompt
