3019e11e1a6585165bb88012c3307724b653c1f0
Andriu <145680244+AndriuMaistryk12@users.noreply.github.com>
2024-06-21 14:10:43 +0300

    Add files via upload

--- javascript_l1.js
+++ javascript_l1.js
@@ -21,7 +21,7 @@ console.log(Pi, height, age, nameTom, nameAnna);
 
 /*Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
 Вивести кожну змінну за допомогою: console.log , alert, document.write*/
-
+/*
 let hello = 'Hi!';
  let word = 'owl';
  let glob_domain = 'domain';
@@ -36,6 +36,38 @@ let hello = 'Hi!';
  let nameTom = true;
  let nameAnna = false; 
  console.log(hello, word, glob_domain, ua_domain, number_one, number_ten, big_minus, numbers);
-console.log(Pi, height, age, nameTom, nameAnna);
+console.log(Pi, height, age, nameTom, nameAnna);*/
 
-prompt
+
+-/*За допомогою 3х різних prompt() отримати 3 слова які являються вашими Ім`ям, По-Батькові та роками.
+  Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".*/
+
+/*let age = prompt("Для реєстрації введіть ваші дані", '');
+console.log (''test);
+
+
+/*За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+  let a = 100; let b = '100'; let c = true;*/
+
+  // чомусь не працює
+  let a = 100; 
+  let b = '100'; 
+  let c = true;
+  console.log(typeof a);
+  console.log(typeof b);
+  console.log(typeof c);
+
+  
+  /*Поставет відповідний оператор в виразах що б вийшов відповідний результат.
+  В однакових виразаї не використовувати однакові оператори!!!*/
+    5 < 6 -> true;
+    5 > 6 -> false;
+    5 == 6 -> false;
+    5 => 6 -> false;
+    10 == 10 -> true;
+    10 === 10 -> true;
+    10 < 10 -> false;
+    10 > 10 -> false;
+    10 != 10 -> false;
+    123 === '123' -> false;
+    123 == '123' -> true;
