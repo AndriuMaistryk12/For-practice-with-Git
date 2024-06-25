4eed841e38c48b111efea7bdf0a7915addf9e38e
Andriu <145680244+AndriuMaistryk12@users.noreply.github.com>
2024-06-25 15:57:05 +0300

    Create js_lab1.js

---
+++ js_lab1.js
@@ -0,0 +1,74 @@
+// створити змінні. Присвоїти кожній значення
+/*
+ let hello = 'hello';
+ let word = 'owu';
+ let glob_domain = 'com';
+ let ua_domain = 'ua'; 
+ let number_one = 1;
+ let number_ten = 10;
+ let big_minus = -999;
+ let numbers = 123;
+ let Pi = 3.14;
+ let height = 2.7;
+ let age = 16;
+ let nameTom = true;
+ let nameAnna = false; 
+
+console.log(hello, word, glob_domain, ua_domain, number_one, number_ten, big_minus, numbers);
+console.log(Pi, height, age, nameTom, nameAnna);*/
+
+
+
+/*Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+Вивести кожну змінну за допомогою: console.log , alert, document.write*/
+
+let hello = 'Hi!';
+ let word = 'owl';
+ let glob_domain = 'domain';
+ let ua_domain = '.ua'; 
+ let number_one = 'one';
+ let number_ten = 'ten';
+ let big_minus = -100;
+ let numbers = 456;
+ let Pi = -3.14;
+ let height = 2.5;
+ let age = 17;
+ let nameTom = true;
+ let nameAnna = false; 
+ console.log(hello, word, glob_domain, ua_domain, number_one, number_ten, big_minus, numbers);
+console.log(Pi, height, age, nameTom, nameAnna)
+
+
+/*За допомогою 3х різних prompt() отримати 3 слова які являються вашими Ім`ям, По-Батькові та роками.
+  Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".*/
+
+/*let age = prompt("Для реєстрації введіть ваші дані", '');
+console.log (''test);*/
+
+
+/*За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+  let a = 100; let b = '100'; let c = true;*/
+
+  
+  let a = 100; 
+  let b = '100'; 
+  let c = true;
+  console.log(typeof a);
+  console.log(typeof b);
+  console.log(typeof c);
+
+  
+  /*Поставет відповідний оператор в виразах що б вийшов відповідний результат.
+  В однакових виразаї не використовувати однакові оператори!!!*/
+    
+  console.log(5 < 6,
+   5 < 6,
+   5 == 6, 
+   5 > 6, 
+   0 == 10,
+   10 == 10,
+   10 < 10,
+   10 > 10,
+    10 != 10,
+    123 === '123',
+    123 == '123');
